d8ade6ec2ad2c5e68d10967d8b1916eb71787845
Yannik Ehlert <kontakt@yanniks.de>
2019-03-29 22:32:24 -0400

    Initial commit

---
+++ MyDemoFramework/Testfile.swift
@@ -0,0 +1,16 @@
+//
+//  Testfile.swift
+//  MyDemoFramework
+//
+//  Created by Yannik Ehlert on 3/29/19.
+//  Copyright © 2019 Yannik Ehlert. All rights reserved.
+//
+
+import UIKit
+
+open class TestUIClass: UIViewController {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello!")
+    }
+}
